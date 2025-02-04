814af429f2770667abd3c1ea249723e2d7e42484
Edugonzalez <eduu.gonzalez@gmail.com>
2025-02-04 13:13:42 +0100

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -1,12 +1,27 @@
 package com.campusdual.classroom;
 
+
+
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0) {
+            throw new DivisionByZeroException("No se puede dividir por cero.");
+        }else{
+        return dividend / divisor;
+        }
     }
 
     public static void main(String[] args) {
+        try {
+            System.out.println("Resultado: " + divisionWithCustomException(3, 0));
+        } catch (DivisionByZeroException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Programa acabado.");
+        }
 
     }
 }
+
+
